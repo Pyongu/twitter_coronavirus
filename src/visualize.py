020cf2208ad7d0d1330aa535d3b03ba2cad68a2d
Pyongu <paulpkim845@gmail.com>
2025-02-14 18:18:33 -0800

    changed vis

--- src/visualize.py
+++ src/visualize.py
@@ -5,6 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_path',required=True)
 parser.add_argument('--key',required=True)
+parser.add_argument('--output_path',required=True)
 parser.add_argument('--percent',action='store_true')
 args = parser.parse_args()
 
@@ -12,6 +13,8 @@ args = parser.parse_args()
 import os
 import json
 from collections import Counter,defaultdict
+import matplotlib.pyplot as plt
+import numpy as np
 
 # open the input path
 with open(args.input_path) as f:
@@ -24,5 +27,21 @@ if args.percent:
 
 # print the count values
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
-for k,v in items:
-    print(k,':',v)
+top10Items = items[10::-1]
+print(top10Items)
+top10CL = []
+top10Num = []
+for i in range(len(top10Items)):
+    kvTuple = items[i]
+    k = kvTuple[0]
+    v = kvTuple[1]
+    top10CL.append(k)
+    top10Num.append(v)
+print(top10CL)
+print(top10Num)
+
+# making bar plot
+plt.bar(top10CL, top10Num, color = 'green')
+plt.title(f'Top 10 Values for { args.key}')
+plt.xlabel(f'{args.input_path}')
+plt.savefig(args.output_path)
